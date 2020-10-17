9f2371d20ed9aebb89410827273f256e052bae36
ColinJWright <colinwright2771@gmail.com>
2020-10-17 14:33:23 +0100

    Initial commit.

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityEngine.XR.WSA.Input;
+
+public class Block : MonoBehaviour
+{
+    [SerializeField] public AudioClip breakSound;
+    [SerializeField] public GameObject blockVFX;
+    [SerializeField] public Sprite[] hitSprites;
+
+    Level level;
+
+    [SerializeField] int timesHit; //serialized for debug so can be seen in Unity
+
+    private void Start()
+    {
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (tag == "Breakable")
+        {
+            HandleHit();
+        }
+
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        if (hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block sprite is missing from array" + gameObject.name);
+        }
+    }
+
+    private void DestroyBlock()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        Destroy(gameObject);
+        level.BlockDestroyed();
+        TriggerVFX();
+        FindObjectOfType<GameSession>().AddToScore();
+    }
+
+    private void TriggerVFX()
+    {
+        GameObject vfx = Instantiate(blockVFX, transform.position, transform.rotation);
+        Destroy(vfx, 1f);
+    }
+}
---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameSession : MonoBehaviour
+{
+    [Range(0.5f, 2.0f)][SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 5;
+    [SerializeField] public TextMeshProUGUI scoreText;
+    [SerializeField] public bool isAutoPlayEnabled;
+
+    [SerializeField] int currentScore = 0;
+
+    // SINGLETON PATTERN - AND CHILDREN OF THE OBJECT ALSO STAY
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameSession>().Length;
+        if (gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Time.timeScale = gameSpeed;
+    }
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+
+    public bool IsAutoPlayEnabled()
+    {
+        return isAutoPlayEnabled;
+    }
+}
